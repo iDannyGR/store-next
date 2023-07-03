801315690aafa3f1c73d8cde2244a5d261a5a061
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-07-03 10:49:33 -0600

    start store

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "plugin:react-hooks/recommended",
+    "next/core-web-vitals",
+    "next",
+    "prettier",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+};
