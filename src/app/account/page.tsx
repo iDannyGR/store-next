1efa0560e4cb6e5f7a2112833ee544dc9def8b88
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-07-03 20:56:59 -0600

    add menu to global

---
+++ src/app/account/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const MyAccount = (props: Props) :React.ReactElement => {
+  return (
+    <div>MyAccount</div>
+  )
+}
+
+export default MyAccount
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import './globals.css'
-import { Chakra_Petch } from 'next/font/google'
+import Navbar from './components/Navbar';
+import './globals.css';
+import { Chakra_Petch } from 'next/font/google';
 
 const inter = Chakra_Petch({subsets:['latin'],weight:['300']})
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   )
 }
