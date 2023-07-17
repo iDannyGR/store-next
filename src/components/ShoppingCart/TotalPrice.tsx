9470443f2552d59752cd9c7da3ffb3bb8b2874ed
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-07-16 20:28:36 -0600

    add button total price

---
+++ src/components/ShoppingCart/TotalPrice.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { GetData } from '@/models/GetData';
+
+type Props = {
+    props:GetData
+}
+
+const TotalPrice = ({}: Props) => {
+    
+  return (
+    <button>
+
+    </button>
+  )
+}
+
+export default TotalPrice
