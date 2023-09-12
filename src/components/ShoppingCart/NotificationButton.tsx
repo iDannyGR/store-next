1ddfbc1bf88f114b3219d5e585dc5fca822d1aed
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-09-12 15:52:20 -0600

    update 12/9/23

--- src/components/ShoppingCart/NotificationButton.tsx
+++ src/components/ShoppingCart/NotificationButton.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 import { ShoppingCartIcon } from '@heroicons/react/24/outline';
 import { ShopingCarStore } from '@/store/ShopingCarStore';
 import { ArticleDetail } from '@/store/ArticleDetailStore';
+import { useHasHydrated } from '@/hooks/useHasHydrated';
+
 
 
 const NotificationButton = ():React.ReactElement=> {
-  
+  const hydration = useHasHydrated()
   const { item, setisOpen, isOpen } = ShopingCarStore();
   const { setisShow } = ArticleDetail();
   
-  const count = Object.values(item).length;
+  const count = Object.keys(item).length > 0 ? Object.values(item).length : 0;
 
     const handleClick = () =>{
       setisOpen(!isOpen);
@@ -20,11 +22,16 @@ const NotificationButton = ():React.ReactElement=> {
 
   
   return (
-      <div className='relative w-12 flex items-center justify-center cursor-pointer ' onClick={handleClick}>
-        <p className='absolute h-6 w-6 flex items-center justify-center rounded-full text-sm top-0 right-0 bg-black text-white'>{count}</p> 
-        <ShoppingCartIcon className="h-8 w-8 text-gray-500" />
-      </div>
-    );
+    <div
+      className="relative flex w-12 cursor-pointer items-center justify-center "
+      onClick={handleClick}
+    >
+      <p className="absolute right-0 top-0 flex h-6 w-6 items-center justify-center rounded-full bg-black text-sm text-white">
+        {hydration  && count}
+      </p>
+      <ShoppingCartIcon className="h-8 w-8 text-gray-500" />
+    </div>
+  );
 }
 
 export default NotificationButton;
