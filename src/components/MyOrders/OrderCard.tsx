4e79c41d9898250579967ca0b9e37f29aaeb7292
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-09-26 20:50:08 -0600

    little update

--- src/components/MyOrders/OrderCard.tsx
+++ src/components/MyOrders/OrderCard.tsx
@@ -11,17 +11,17 @@ type OrderProps = {
 const OrderCard = ({order}: OrderProps) => {
   return (
     <div
-      className="relative flex h-32 w-[450px] rounded-lg bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
+      className="relative grid h-32 w-[450px] grid-cols-3 grid-rows-3 gap-1 items-center text-center rounded-lg bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
       key={order.id}
     >
-      <p className='absolute top-0 font-semibold'>Order Number: {order.id.toUpperCase()}</p>
+      <p className="col-span-3 font-semibold">Order Number: {order.id.toUpperCase()}</p>
       <GoToOrder id={order.id} />
-      <BanknotesIcon className="h-6 w-6" />
-      <p>${order.totalPrice}</p>
-      <p>Total of Items: {order.totalProducts}</p>
-      <ListBulletIcon className="h-6 w-6" />
-      <p>{order.date.split(',')[0]}</p>
-      <CalendarDaysIcon className="h-6 w-6" />
+      <BanknotesIcon className="row-start-2 w-6" />
+      <ListBulletIcon className="row-start-2 w-6" />
+      <CalendarDaysIcon className="row-start-2 w-6" />
+      <p className="row-start-3">${order.totalPrice}</p>
+      <p className="row-start-3">Total of Items: {order.totalProducts}</p>
+      <p className="row-start-3">{order.date.split(',')[0]}</p>
     </div>
   );
 };
