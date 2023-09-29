818198d049e6f8364482bceacde5a0f35f0b4ef2
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-09-28 21:42:21 -0600

    fixed get data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
 import Card from '@/components/Card';
-import { GetData } from '@/models/GetData';
 
-async function getData(): Promise<GetData[]> {
-  const response = await fetch('https://fakestoreapi.com/products');
-  const data = await response.json();
 
-  if (response.ok) {
-    // Validar y tipar los datos utilizando la interfaz GetData
-    const products: GetData[] = data;    
-    return products;
-  } else {
-    throw new Error('Error al obtener los datos');
-  }
-}
  export default async function Home() {
  const data = await getData()
   return (
---
+++ src/utils/GetData.ts
@@ -0,0 +1,31 @@
+import { GetData } from '@/models/GetData';
+
+async function getData(): Promise<GetData[]> {
+  const response = await fetch('https://fakestoreapi.com/products');
+  const data = await response.json();
+
+  if (response.ok) {
+    // Validar y tipar los datos utilizando la interfaz GetData
+    const products: GetData[] = data;
+    return products;
+  } else {
+    throw new Error('Error al obtener los datos');
+  }
+}
+async function getCategory(category?:string):Promise<Getdata[]> {
+    
+    if(category){
+        const response = await fetch(`https://fakestoreapi.com/products/category/${category}`)
+    }else{
+        const response = await fetch('https://fakestoreapi.com/products');
+    }
+    
+    const data =  await response.json();
+     if (response.ok) {
+       // Validar y tipar los datos utilizando la interfaz GetData
+       const products: GetData[] = data;
+       return products;
+     } else {
+       throw new Error('Error al obtener los datos');
+     }
+}
