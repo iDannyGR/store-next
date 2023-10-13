8d74515fda1ef7060e15cb5bcbf6da55281bbb19
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-10-12 20:55:35 -0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Card from '@/components/Card';
 import { getData } from '@/utils/GetData';
 
- export default async function Home() {
- const data = await getData()
-  return (
-    <article className="grid grid-cols-4 gap-12">
-      {data && data.map(product => (
-        <Card product={product} />
-      ))}
-    </article>
-  );
-}
+ export default async function Home({ searchParams }) {
+   console.log(searchParams);
+   const data = await getData();
+   return (
+     <article className="grid grid-cols-4 gap-12">
+       {data && data.map((product) => <Card product={product} />)}
+     </article>
+   );
+ }
