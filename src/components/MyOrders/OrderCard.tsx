0af5f57560fd9c15db155ef38d1aa2b188b9074b
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-11-21 15:32:22 -0600

    update card orders

--- src/components/MyOrders/OrderCard.tsx
+++ src/components/MyOrders/OrderCard.tsx
@@ -10,16 +10,19 @@ type OrderProps = {
 
 const OrderCard = ({order}: OrderProps) => {
   return (
-    <div
-      className="relative grid h-32 w-[450px] grid-cols-3 grid-rows-3 gap-1 items-center text-center rounded-lg bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-      <p className="col-span-3 font-semibold">Order Number: {order.id.toUpperCase()}</p>
-      <GoToOrder id={order.id} />
-      <BanknotesIcon className="row-start-2 w-6" />
-      <ListBulletIcon className="row-start-2 w-6" />
-      <CalendarDaysIcon className="row-start-2 w-6" />
-      <p className="row-start-3">${order.totalPrice}</p>
-      <p className="row-start-3">Total of Items: {order.totalProducts}</p>
-      <p className="row-start-3">{order.date.split(',')[0]}</p>
+    <div className="relative flex flex-col justify-center items-center h-32 w-[450px] rounded-lg bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <GoToOrder id={order.id} />
+        <p className="text-center font-semibold">Order Number: {order.id.toUpperCase()}</p>
+        <div className='flex h-12 w-[65%] justify-between'>
+          <BanknotesIcon className="w-6" />
+          <ListBulletIcon className="w-6" />
+          <CalendarDaysIcon className="w-6" />
+        </div>
+        <div className='flex w-[75%] justify-between'>
+          <p className="row-start-3">${order.totalPrice}</p>
+          <p className="row-start-3">Total of Items: {order.totalProducts}</p>
+          <p className="row-start-3">{order.date.split(',')[0]}</p>
+        </div>
     </div>
   );
 };
